7eda01f663e4a1198fc4f68e746aceb1f0e0bfc2
GM <ghouga.m@gmail.com>
2018-02-19 16:04:19 +0100

    Initial commit

---
+++ app/src/main/java/com/udacitycard/android/spacecat/MainActivity.java
@@ -0,0 +1,20 @@
+package com.udacitycard.android.spacecat;
+
+import android.graphics.Matrix;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_c);
+
+
+    }
+}
